9ef8d06378587f0392bcfebb3cf4a2ad764c5aa0
Olivia <oliviadodge@gmail.com>
2021-11-01 22:50:34 -0700

    Some refactoring

--- app/src/main/java/com/develop/connection/ui/home/HomeFragment.kt
+++ app/src/main/java/com/develop/connection/ui/home/HomeFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
@@ -16,11 +15,7 @@ import dagger.hilt.android.AndroidEntryPoint
 class HomeFragment : Fragment() {
 
     private lateinit var viewModel: HomeViewModel
-    private var _binding: FragmentHomeBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
+    private lateinit var binding: FragmentHomeBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -28,17 +23,12 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         viewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        setupBinding()
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        setupView()
         return binding.root
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
-
-    private fun setupBinding() {
+    private fun setupView() {
         viewModel.text.observe(viewLifecycleOwner, {
             binding.textHome.text = it
         })
